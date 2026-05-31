93a370bd19e418b6902fa0737e1168f8b2469909
Obinna Uche <75355071+ObiUche@users.noreply.github.com>
2026-05-31 14:53:56 +0100

    slight improvement

--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,38 @@
 public class Solution {
 
-    public static boolean isPalindrome(String s){
-        String trimedString = s.replace(" ", "").toLowerCase();
-        String finalString = trimedString.replaceAll("\\p{Punct}", "");
-        char[] array = finalString.toCharArray();
+    public static boolean isPalindrome(String s) {
+        // first i am going to remove the spaces
+        // slightly more efficent but reassigning strings is the same as creating another string
+        s = s.replaceAll(" ", "").toLowerCase();
+        // Now remove punctuation
+        s = s.replaceAll("\\p{Punct}", "");
+        char[] array = s.toCharArray();
+
+        // Set the pointers to beginning and end of array
+        int Left = 0, Right = array.length - 1;
         boolean matched = false;
-        // Two Pointers
-        int Left = 0, Right = array.length -1;
 
-        while(Left < Right){
+        while (Left < Right) {
 
-            // if both chars match continue
-            if(array[Left] == array[Right]){
+            if (array[Left] == array[Right]) {
                 matched = true;
-                Left ++;
-                Right --;
+                Left++;
+                Right--;
             } else {
                 matched = false;
                 break;
             }
+
         }
 
         return matched;
 
     }
 
+
     public static void main(String[] args) {
-       String S = "tab a cat";
-       System.out.println(isPalindrome(S));
+        String S = "tab a cat";
+        System.out.println(isPalindrome(S));
 
     }
 }
